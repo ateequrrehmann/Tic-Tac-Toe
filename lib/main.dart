30e05e28750db77133748bc8280e05a81abb109b
Ateeq Ur Rehman <rehmanateequr501@gmail.com>
2024-05-13 08:47:21 +0500

    Initial Commit HomeScreen

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: Home(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Tic Tac Toe"),
+      ),
+    );
+  }
+}
+
